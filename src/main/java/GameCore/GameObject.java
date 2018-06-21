a52a4a25b001ae604ef4e1f5ef6e8cbcbbc86f44
jeffrey <haruv1070@gmail.com>
2018-06-21 19:44:32 +0300

    .

--- src/main/java/GameCore/GameObject.java
+++ src/main/java/GameCore/GameObject.java
@@ -7,4 +7,5 @@ public class GameObject extends JLabel {
 * this class gather all object in the game to gameObject
  * in order to know who is an object of the world and who is other object
 * */
+
 }
